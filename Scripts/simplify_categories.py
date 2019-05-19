27076d708f2d9b296057fa3bea2dc03af1f43b58
Henkkis <40563680+Henkkis@users.noreply.github.com>
2019-05-19 13:37:57 +0300

    Changed category enumeration to be in line with Pytorch documentation for Nlllos

--- Scripts/simplify_categories.py
+++ Scripts/simplify_categories.py
@@ -1,47 +1,47 @@
-# Physics 1 [ 1 0 0 0 0 0]
-# Mathematics 2 [ 0 1 0 0 0 0 ]
-# Computer Science 3 [ 0 0 1 0 0 0]
-# Quantative biology 4 [ 0 0 0 1 0 0]
-# Quantative finance 5 [ 0 0 0 0 1 0]
-# Statistic 6 [ 0 0 0 0 0 1]
+# Physics 0 [ 1 0 0 0 0 0]
+# Mathematics 1 [ 0 1 0 0 0 0 ]
+# Computer Science 2 [ 0 0 1 0 0 0]
+# Quantative biology 3 [ 0 0 0 1 0 0]
+# Quantative finance 4 [ 0 0 0 0 1 0]
+# Statistic 5 [ 0 0 0 0 0 1]
 import sys
 simple ={}
 
-simple["acc-phys"] = 1
-simple["adap-org"] = 2
-simple["alg-geom"] = 2
-simple["astro-ph"] = 1
-simple["atom-ph"] = 1
-simple["chao-dyn"] =2
-simple["chem-ph"] = 1
-simple["cmp-lg"] = 3
-simple["comp-gas"] = 2
-simple["cond-mat"] = 1
-simple["cs"] = 3
-simple["dg-ga"] = 2
-simple["econ"] = 5
-simple["eess"] = 1
-simple["funct-an"] = 2
-simple["gr-qc"] = 1
-simple["hep-ex"] = 1
-simple["hep-lat"] = 1
-simple["hep-ph"] = 1
-simple["hep-th"] = 1
-simple["math"] = 2
-simple["math-ph"] = 2
-simple["mtrl-th"] = 1
-simple["nlin"] = 2
-simple["nucl-ex"] = 1
-simple["nucl-th"] = 1
-simple["patt-sol"] = 2 
-simple["physics"] = 1
-simple["q-alg"] = 2
-simple["q-bio"] = 4
-simple["q-fin"] = 5
-simple["quant-ph"] = 1 
-simple["solv-int"] = 2
-simple["stat"] = 6
-simple["supr-con"] = 1
+simple["acc-phys"] = 0
+simple["adap-org"] = 1
+simple["alg-geom"] = 1
+simple["astro-ph"] = 0
+simple["atom-ph"] = 0
+simple["chao-dyn"] =1
+simple["chem-ph"] = 0
+simple["cmp-lg"] = 2
+simple["comp-gas"] = 1
+simple["cond-mat"] = 0
+simple["cs"] = 2
+simple["dg-ga"] = 1
+simple["econ"] = 4
+simple["eess"] = 0
+simple["funct-an"] = 1
+simple["gr-qc"] = 0
+simple["hep-ex"] = 0
+simple["hep-lat"] = 0
+simple["hep-ph"] = 0
+simple["hep-th"] = 0
+simple["math"] = 1
+simple["math-ph"] = 1
+simple["mtrl-th"] = 0
+simple["nlin"] = 1
+simple["nucl-ex"] = 0
+simple["nucl-th"] = 0
+simple["patt-sol"] = 1
+simple["physics"] = 0
+simple["q-alg"] = 1
+simple["q-bio"] = 3
+simple["q-fin"] = 4
+simple["quant-ph"] = 0
+simple["solv-int"] = 1
+simple["stat"] = 5
+simple["supr-con"] = 0
 
 
 data_dir=sys.argv[1]
